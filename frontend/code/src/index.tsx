71292a140bf2e0f23933624e32b4780d7f6e0bee
ismail <ismailkharrobi1@gmail.com>
2023-08-19 17:53:39 +0100

    ci/cd

---
+++ frontend/code/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>staging and production</h1>
+    </div>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
