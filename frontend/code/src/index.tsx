d9120f9ed62dd4dc875e555a2cde7e5f7189c4b5
ismail <ismailkharrobi1@gmail.com>
2023-08-20 10:56:23 +0100

    front init

--- frontend/code/src/index.tsx
+++ frontend/code/src/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
+import './index.css';
+import {Login} from './login'
+import {Main} from './main'
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>staging and production</h1>
-    </div>
+    {/* <img src={bg_landing} alt="" /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/main" element={<Main/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
