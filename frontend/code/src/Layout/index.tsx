4d384fa85e49bfceb0b96358bd304f784ef83b37
ismail <ismailkharrobi1@gmail.com>
2023-08-22 10:47:46 +0100

    fix_workflow

--- frontend/code/src/Layout/index.tsx
+++ frontend/code/src/Layout/index.tsx
@@ -5,7 +5,7 @@ export const Layout =  () =>
     return (
     <div className='h-screen bg-base-200'>
         <div className= " flex h-18 bg-base-100">
-            <img src={logo} className='p-3 w-24 '></img>
+            <img src={logo} alt="logo" className='p-3 w-24 '></img>
         </div>
         <div className='h-screen bg-base-100 w-20 '>
         </div>
