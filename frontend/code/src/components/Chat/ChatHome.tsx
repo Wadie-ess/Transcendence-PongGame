e2d1a679b626371576f1a6acf658cbdbd88d2c63
Wadie-ess <walidehanin@gmail.com>
2023-09-01 15:55:19 +0100

    init Chat layout

---
+++ frontend/code/src/components/Chat/ChatHome.tsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import { Layout } from '../Layout';
+
+const Chat = () => {
+  return (
+    <>
+   <Layout>
+    <h1 >
+        chat goes here hhh 
+    </h1>
+   </Layout>
+    </>
+  );
+}
+
+export default Chat;
