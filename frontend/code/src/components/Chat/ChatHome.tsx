db6f7c7212f2e31c27f28dd31ea4b681fc1383a6
Wadie-ess <walidehanin@gmail.com>
2023-09-11 12:14:09 +0100

    finish flexible  user preview

--- frontend/code/src/components/Chat/ChatHome.tsx
+++ frontend/code/src/components/Chat/ChatHome.tsx
@@ -10,6 +10,8 @@ import SearchIcon from "./assets/Search_icon.svg";
 import EditIcon from "./assets/Edit_icon.svg";
 import More from "./assets/more_icon.svg";
 import Send from "./assets/send_icon.svg";
+import Close from "./assets/close_icon.svg";
+import Bio from "./assets/bio_icon.svg";
 import { useState } from "react";
 import {
   CurrentUserMessage,
@@ -17,25 +19,34 @@ import {
   UserMessage,
 } from "./Components/MessageHelpers";
 
-// for test
-
-const RowComponent3 = () => {
-  return <div className="bg-black">{/* Your content for Row 1 */}</div>;
-};
+interface ConversationProps {
+  onRemoveUserPreview: () => void; // Define the prop to accept the function
+}
 
 const Chat = () => {
+  const [showUserPreview, setShowUserPreview] = useState(true);
+
+  const handleRemoveUserPreview = () => {
+    setShowUserPreview(!showUserPreview);
+  };
   return (
     <>
       <Layout>
-        <div className="flex h-full  divide-black divide-x-4">
-          <div className="w-4/12">
-            <RecentMessages />
+        <div className="flex h-full divide-black divide-x-4">
+          <div className={` ${showUserPreview ? "w-4/12" : "w-4/12"}`}>
+            {<RecentConversations />}
           </div>
-          <div className="w-6/12 overflow-hidden bg-gray-900">
-            <Conversation />
+          <div
+            className={` ${
+              showUserPreview ? "w-6/12" : "w-8/12"
+            } overflow-hidden bg-gray-900`}
+          >
+            <Conversation onRemoveUserPreview={handleRemoveUserPreview} />
           </div>
-          <div className="w-3/12 bg-[#1A1C26]">
-            <RowComponent3 />
+          <div className={` ${showUserPreview ? "w-3/12" : ""}  bg-[#1A1C26]`}>
+            {showUserPreview && (
+              <UserPreviewCard onRemoveUserPreview={handleRemoveUserPreview} />
+            )}
           </div>
         </div>
       </Layout>
@@ -43,12 +54,48 @@ const Chat = () => {
   );
 };
 
-export const ConversationHeader = () => {
+export const UserPreviewCard: React.FC<ConversationProps> = ({
+  onRemoveUserPreview,
+}) => {
+  return (
+    <div className="flex flex-col p-4 ">
+      <div className="flex flex-row justify-between ">
+        <p className="text-white font-poppins font-light text-base">
+          Person Info
+        </p>
+        <button onClick={onRemoveUserPreview}>
+          <img alt="" src={Close} />
+        </button>
+      </div>
+      <div className="flex flex-row justify-center p-4">
+        <img className="w-36 rounded-full " alt="" src={yas} />
+      </div>
+      <div className="flex flex-row justify-center p-1 text-gray-400 font-poppins font-medium text-base">
+        <p>Friend</p>
+      </div>
+      <div className="flex flex-row  text-gray-400 font-poppins font-medium text-base ">
+        <img alt="" src={Bio} />
+        <p className="pl-2">Yassin's Bio</p>
+      </div>
+      <div className=" bg-[#1A1C26]">
+        <p className="text-white  p-1 pt-2 font-poppins font-normal whitespace-normal overflow-auto break-words">
+          hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export const ConversationHeader: React.FC<ConversationProps> = ({
+  onRemoveUserPreview,
+}) => {
   return (
     <>
-      <div className="flex flex-row justify-between bg-[#1A1C26] p-4 border-b-2  border-black border- ">
+      <div className="flex flex-row justify-between bg-[#1A1C26] p-3 border-b-2  border-black  ">
         <div className="flex flex-row ">
-          <img src={yas} className="w-[18%] h-[95%]" />
+          <div className="pr-1">
+            <img className="w-12 rounded-full " alt="" src={yas} />
+          </div>
           <div className="flex flex-col pl-2 ">
             <p className="text-white font-poppins text-base font-medium leading-normal">
               {" Yassine Alaoui"}
@@ -58,16 +105,52 @@ export const ConversationHeader = () => {
             </p>
           </div>
         </div>
-        <details className="relative pt-2">
-          <summary className="list-none">
+        {/* <div className="dropdown pt-2">
+          <label tabIndex={0} className="list-none  cursor-pointer">
             <img src={More} alt="More" />
-          </summary>
-          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 absolute  right-full  ">
+          </label>
+          <ul className="shadow menu p-2 dropdown-content z-[1] bg-base-100 rounded-box w-52 absolute  right-full">
             <li>
-              <a className="hover:bg-[#7940CF]">Block</a>
+              <a href="gg" className="hover:bg-[#7940CF]">
+                Block
+              </a>
             </li>
             <li>
-              <a className="hover:bg-[#7940CF]">Unfriend</a>
+              <a href="gg" className="hover:bg-[#7940CF]">
+                Unfriend
+              </a>
+            </li>
+            <li onClick={onRemoveUserPreview}>
+              <a
+                onClick = {
+                  ()=> console.log("hhh")
+                }
+                className="hover:bg-[#7940CF]"
+              >
+                Show User Info
+              </a>
+            </li>
+          </ul>
+        </div> */}
+        <details  className="relative ">
+          <summary className="list-none p-3 cursor-pointer ">
+            <img src={More} alt="More" />
+          </summary>
+          <ul  className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 absolute  right-full  ">
+            <li>
+              <a href="gg" className="hover:bg-[#7940CF]">
+                Block
+              </a>
+            </li>
+            <li>
+              <a href="gg" className="hover:bg-[#7940CF]">
+                Unfriend
+              </a>
+            </li>
+            <li>
+              <a onClick={onRemoveUserPreview} className="hover:bg-[#7940CF]">
+                Show User Info
+              </a>
             </li>
           </ul>
         </details>
@@ -78,34 +161,116 @@ export const ConversationHeader = () => {
 
 export const MessageTextInput = () => {
   return (
-    <div className="flex flex-row  m-5 justify-evenly">
+    <div className="flex flex-row  m-5 justify-evenly ">
       <input
         type="text"
         placeholder="Type Message"
         className="input w-full shadow-md max-w-lg bg-[#1A1C26]  placeholder:text-gray-400 font-poppins text-base font-normal leading-normal "
       />
 
-      <button className="btn btn-square bg-[#8C67F6]">
+      <button className="btn  ml-4 btn-square  bg-[#8C67F6]">
         <img src={Send} />
       </button>
     </div>
   );
 };
 
-export const Conversation = () => {
+export const Conversation: React.FC<ConversationProps> = ({
+  onRemoveUserPreview,
+}) => {
+  const [Messages, SetMessages] = useState([
+    {
+      userImage: UserImage,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas2,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: true,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+    {
+      userImage: yas1,
+      username: "a7a 1",
+      message: "a7a test",
+      time: "6:00",
+      isRead: true,
+      isMe: false,
+    },
+  ]);
   return (
-    <div className="flex flex-col ">
-      <ConversationHeader />
-      <UserMessage />
-      <CurrentUserMessage />
-      <MessageTextInput />
+    <div className="flex flex-col h-[90%] ">
+      <ConversationHeader onRemoveUserPreview={onRemoveUserPreview} />
+      <div className="flex-grow overflow-y-auto no-scrollbar">
+        {Messages.map((message) =>
+          message.isMe ? <CurrentUserMessage /> : <UserMessage />
+        )}
+      </div>
+
+      <div className="fixed bottom-2  mx-auto w-[40%]">
+        <MessageTextInput />
+      </div>
     </div>
   );
 };
 
 // to make it dynamic list later !
-export const RecentMessages = () => {
-  const [Messages, SetMessages] = useState([
+export const RecentConversations = () => {
+  const [Conversation, SetConversation] = useState([
     {
       userImage: UserImage,
       username: "a7a 1",
@@ -184,7 +349,7 @@ export const RecentMessages = () => {
     <div className="h-full flex flex-col">
       <OnlineNowUsers />
       <div className="flex-grow overflow-y-auto no-scrollbar">
-        {Messages.map((message) => (
+        {Conversation.map((message) => (
           <MessagePlaceHolder
             key={message.userImage}
             username={message.username}
--- frontend/code/src/components/Chat/Components/MessageHelpers.tsx
+++ frontend/code/src/components/Chat/Components/MessageHelpers.tsx
@@ -1,64 +1,44 @@
 import Send from "../assets/send_icon.svg";
 import yas from "../assets/alaoui.png";
 
+interface myConversationProps {
+  username: string;
+  message: string;
+  time: string;
+  isMe: boolean;
+  isRead: boolean;
+  userImage: string;
+}
 
-
-
-
-
-
-
-
-
-interface myMessageProps {
-    username: string;
-    message: string;
-    time: string;
-    isMe: boolean;
-    isRead: boolean;
-    userImage: string;
-  }
-
-
-
-
-
-
-
-
-
-
-
-  export const CurrentUserMessage = () => {
-    return (
-      <div className="chat chat-end p-5">
-        <div className="chat-bubble bg-purple-500 text-white">nn hh </div>
-      </div>
-    );
-  };
-  export const UserMessage = () => {
-    return (
-      <>
-        <div className="chat chat-start p-5">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img src={yas} />
-            </div>
-          </div>
-          <div className="chat-header p-1">
-            <time className="text-gray-400 font-poppins text-xs font-light leading-normal">
-              12:45 PM
-            </time>
-          </div>
-          <div className="chat-bubble">{"Fiha chi Kharja l Marakech?"}</div>
-          <div className="chat-footer p-1 text-gray-400 font-poppins text-xs font-light leading-normal">
-            Delivered
+export const CurrentUserMessage = () => {
+  return (
+    <div className="chat chat-end p-3">
+      <div className="chat-bubble bg-purple-500 text-white">nn hh </div>
+    </div>
+  );
+};
+export const UserMessage = () => {
+  return (
+    <>
+      <div className="chat chat-start p-3">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src={yas} />
           </div>
         </div>
-      </>
-    );
-  };
-
+        <div className="chat-header p-1">
+          <time className="text-gray-400 font-poppins text-xs font-light leading-normal">
+            12:45 PM
+          </time>
+        </div>
+        <div className="chat-bubble">{"Salam !"}</div>
+        <div className="chat-footer p-1 text-gray-400 font-poppins text-xs font-light leading-normal">
+          Delivered
+        </div>
+      </div>
+    </>
+  );
+};
 
 export const MessageTextInput = () => {
   return (
@@ -76,43 +56,42 @@ export const MessageTextInput = () => {
   );
 };
 
-
 export const MessagePlaceHolder = ({
-    username,
-    message,
-    time,
-    isRead,
-    userImage,
-  }: myMessageProps) => {
-    return (
-      <div className="message-container flex   pt-5 pl-5 pb-5 pr-2 hover:bg-[#272932]  bg-[#1A1C26] ">
-        <div className="user-image flex-shrink-0 mr-2">
-          <img
-            className="user-image h-10 w-10 rounded-full"
-            src={userImage}
-            alt={`second's Profile`}
-          />
+  username,
+  message,
+  time,
+  isRead,
+  userImage,
+}: myConversationProps) => {
+  return (
+    <div className="message-container flex   pt-5 pl-5 pb-5 pr-2 hover:bg-[#272932]  bg-[#1A1C26] ">
+      <div className="user-image flex-shrink-0 mr-2">
+        <img
+          className="user-image h-10 w-10 rounded-full"
+          src={userImage}
+          alt={`second's Profile`}
+        />
+      </div>
+      <div className="message-colum align-middle flex flex-col flex-grow">
+        <div className="message-row flex flex-row justify-between">
+          <p className="text-white font-poppins text-base font-normal leading-normal ">
+            {username}
+          </p>
+          <p className="text-gray-400 font-poppins text-sm font-light leading-normal ">
+            {time} PM
+          </p>
         </div>
-        <div className="message-colum align-middle flex flex-col flex-grow">
-          <div className="message-row flex flex-row justify-between">
-            <p className="text-white font-poppins text-base font-normal leading-normal ">
-              {username}
-            </p>
-            <p className="text-gray-400 font-poppins text-sm font-light leading-normal ">
-              {time} PM
-            </p>
-          </div>
-          <div className=" flex flex-row justify-between">
-            <p className="text-gray-400 font-poppins text-sm font-medium leading-normal ">
-              {message}
-            </p>
-            <div className="messages-dot relative inline-flex">
-              <div className="w-4 h-4 bg-red-500 rounded-full text-white flex items-center justify-center">
-                <span className="text-xs  font-medium">5</span>
-              </div>
+        <div className=" flex flex-row justify-between">
+          <p className="text-gray-400 font-poppins text-sm font-medium leading-normal ">
+            {message}
+          </p>
+          <div className="messages-dot relative inline-flex">
+            <div className="w-4 h-4 bg-red-500 rounded-full text-white flex items-center justify-center">
+              <span className="text-xs  font-medium">5</span>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
