1c460d70213b7912c648ecc506b85eff97b18125
Ismail Kharrobi <ismailkharrobi1@gmail.com>
2023-09-15 23:58:02 +0100

    OPTIMIZE:nginx server (#31)
    * ADD:gzip
    FIX:nginx
    
    * ADD:dynamic settings

--- frontend/code/src/Components/Settings/index.tsx
+++ frontend/code/src/Components/Settings/index.tsx
@@ -3,33 +3,39 @@ import { Edit } from './assets/Edit'
 import  Newbie  from '../Badges/Newbie.svg'
 import  Master  from '../Badges/Master.svg'
 import  Ultimate  from '../Badges/Ultimate.svg'
-import { api }from '../../Api/base'
+import { api as axsios }from '../../Api/base'
 import toast from 'react-hot-toast'
 import { useContext ,  useState , useEffect} from 'react'
 import { userContext } from '../../Context'
-
+import { Inputs } from './assets/Inputs'
 export const Setting = () => { 
     const getdata :any = async() => {
-            const data:any = await api.get("/test")
-          return data
+        const data:any = await axsios.get("/test")
+        return data
     }
+    
     const {user} :any = useContext(userContext);
     const [myuser, setMyuser] = useState(user);
+    const data_names = ["First name","Last name","Email"]
+    const data_content = [user.name.first,user.name.last, user.email]
+
     useEffect(()=>{
         setMyuser(user)
     },[myuser, user])
+
+  
     return (
             
            <>
             
-           <div className="flex h-[90vh] w-full  ">
+           <div className="flex h-[90vh] w-full font-poppins font-medium">
                 <h1 className='pt-6 pl-6 font-poppins font-medium text-xl text-neutral'>Profile Settings</h1>
                 <div className=" h-[82%] w-[90%] flex flex-col absolute bottom-0 right-0 bg-base-200 rounded-tl-2xl">
-                    <h2 className='pt-4 pl-4 text-neutral'>change preview</h2>
+                    <h2 className='pt-4 pl-4 text-neutral '>change preview</h2>
                     <div className="flex justify-center h-full w-full pt-8">
-                        <div className="flex flex-col sm:flex-row sm:flex-wrap items-center gap-4 justify-between overflow-auto  w-[90%]  max-h-[20vh]  bg-base-100 border-solid border-gray-400 border-2  rounded-3xl">
-                            <div className='flex justify-between items-center gap-x-10 px-2 sm:px-0'>
-                                <div className='relative sm:pl-10 pt-2 sm:pt-0'><Avatar picture={myuser.picture.medium}/>
+                        <div className="flex flex-col sm:flex-row  items-center gap-4 sm:justify-between justify-center overflow-scroll no-scrollbar  w-[90%]  max-h-[25vh]  bg-base-100 border-solid border-gray-400 border-2  rounded-3xl">
+                            <div className='flex justify-between items-center  gap-x-10 px-2 sm:px-0'>
+                                <div className='relative sm:pl-10 pt-6 sm:pt-0'><Avatar picture={myuser.picture.medium}/>
                                     <div className="absolute bottom-0 right-0">
                                         <Edit/>
                                     </div>
@@ -52,9 +58,9 @@ export const Setting = () => {
                  }
                );}}>
                         </div>
-                                <img className={`h-[9vh] sm:h-[14vh] `} src={Newbie} alt="newbie badge" />
-                                <img className={`h-[9vh] sm:h-[14vh] opacity-30`} src={Master} alt="Master badge" />
-                                <img className={`h-[9vh] sm:h-[14vh] `} src={Ultimate} alt="Ultimate badge" />
+                                <img className={`h-[9vh] md:h-[12vh] `} src={Newbie} alt="newbie badge" />
+                                <img className={`h-[9vh] md:h-[12vh] opacity-30`} src={Master} alt="Master badge" />
+                                <img className={`h-[9vh] md:h-[12vh] `} src={Ultimate} alt="Ultimate badge" />
                             </div>
                         </div>
                     </div>
@@ -62,10 +68,16 @@ export const Setting = () => {
                 <h2 className='pt-4 pl-4 text-neutral'>change preview</h2>
                     <div className="flex justify-center h-full w-full pt-8">
                         <div className="flex flex-col sm:flex-row justify-between w-[90%] h-[50vh] sm:max-h-[50vh] max-h-[40vh] bg-base-100 border-solid border-gray-400 border-2  rounded-3xl">
-                            <div className="flex justify-start  items-start pt-4 pl-4">
-                                <h6 className='text-sm'>name</h6>
-                                
-                            </div>
+                                <div className='gap-4 p-6 flex-wrap shrink grid grid-cols-2 justify-between w-full h-56'>
+                                {
+                                    data_names.map((x, index) => {
+                                        return (
+                                            <Inputs key={index} name={x} data={data_content[index]} />
+                                            ) 
+                                        })
+                                        
+                                }
+                                </div>
                         </div>
                     </div>
                             
