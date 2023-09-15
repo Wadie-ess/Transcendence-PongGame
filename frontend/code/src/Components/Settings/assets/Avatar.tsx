75bd7e055cab69dd2a3eab170acb573b4ff75e8a
ismail <ismailkharrobi1@gmail.com>
2023-09-15 23:51:04 +0100

    ADD:dynamic settings

--- frontend/code/src/Components/Settings/assets/Avatar.tsx
+++ frontend/code/src/Components/Settings/assets/Avatar.tsx
@@ -1,7 +1,7 @@
 export const Avatar = (props:any) => {
     return (
         <div className="avatar">
-        <div className="w-24 rounded-full">
+        <div className="w-16 sm:w-24 rounded-full">
             <img src={props.picture} alt="avatar" />
         </div>
         </div>
