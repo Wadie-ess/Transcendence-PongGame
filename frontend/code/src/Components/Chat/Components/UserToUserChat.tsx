d328f96e454d6f1055d5bf472d127aad31375eaa
Wadie-ess <walidehanin@gmail.com>
2023-09-30 16:39:47 +0100

    interactive chat Layout

--- frontend/code/src/Components/Chat/Components/UserToUserChat.tsx
+++ frontend/code/src/Components/Chat/Components/UserToUserChat.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Conversation, UserPreviewCard } from "..";
 
-import { create } from 'zustand'
 
 
 
@@ -14,48 +13,48 @@ import { create } from 'zustand'
 
 
 
-type State = {
-  firstName: string
-  lastName: string
-}
+// type State = {
+//   firstName: string
+//   lastName: string
+// }
 
-type Action = {
-  updateFirstName: (firstName: State['firstName']) => void
-  updateLastName: (lastName: State['lastName']) => void
-}
+// type Action = {
+//   updateFirstName: (firstName: State['firstName']) => void
+//   updateLastName: (lastName: State['lastName']) => void
+// }
 
 // Create your store, which includes both state and (optionally) actions
-const usePersonStore = create<State & Action>((set) => ({
-  firstName: '',
-  lastName: '',
-  updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
-  updateLastName: (lastName) => set(() => ({ lastName: lastName })),
-}))
+// const usePersonStore = create<State & Action>((set) => ({
+//   firstName: '',
+//   lastName: '',
+//   updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+//   updateLastName: (lastName) => set(() => ({ lastName: lastName })),
+// }))
 
-// In consuming app
-function App() {
-  // "select" the needed state and actions, in this case, the firstName value
-  // and the action updateFirstName
-  const firstName = usePersonStore((state) => state.firstName)
-  const updateFirstName = usePersonStore((state) => state.updateFirstName)
+// // In consuming app
+// function App() {
+//   // "select" the needed state and actions, in this case, the firstName value
+//   // and the action updateFirstName
+//   const firstName = usePersonStore((state) => state.firstName)
+//   const updateFirstName = usePersonStore((state) => state.updateFirstName)
 
-  return (
-    <main>
-      <label>
-        First name
-        <input
-          // Update the "firstName" state
-          onChange={(e) => updateFirstName(e.currentTarget.value)}
-          value={firstName}
-        />
-      </label>
+//   return (
+//     <main>
+//       <label>
+//         First name
+//         <input
+//           // Update the "firstName" state
+//           onChange={(e) => updateFirstName(e.currentTarget.value)}
+//           value={firstName}
+//         />
+//       </label>
 
-      <p>
-        Hello, <strong>{firstName}!</strong>
-      </p>
-    </main>
-  )
-}
+//       <p>
+//         Hello, <strong>{firstName}!</strong>
+//       </p>
+//     </main>
+//   )
+// }
 
 
 export const UserToUserChat = () => {
