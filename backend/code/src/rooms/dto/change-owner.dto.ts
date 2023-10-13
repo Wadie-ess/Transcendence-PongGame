61ef176694428478b0c93be65415a124ea35fa3d
Ayoub Essabiry <aessabir@AbdelaaliEsSalmis-iMac.local>
2023-10-13 13:20:14 +0100

    swagger for changeRoomOwner

--- backend/code/src/rooms/dto/change-owner.dto.ts
+++ backend/code/src/rooms/dto/change-owner.dto.ts
@@ -1,14 +1,15 @@
-// import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
 } from 'class-validator';
 
 export class ChangeOwnerDto{
-    // @ApiProperty()
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     roomId: string;
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     NewOwnerId: string;
